4aaff23a4edcc5906cbedfa79193ea2e2838f22d
lanc3r_YT <pachecolau27@gmail.com>
2024-04-02 15:03:17 -0500

    form validation completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,11 @@
 const contactForm = document.querySelector('#formContact');
 const dataUser = contactForm.querySelectorAll('input');
+const btnSend = contactForm.querySelector('#btnSubmit');
 
 const emailToSend = {
     "firstName": "",
     "lastName": "",
-    "userAddress": "",
+    "userEmail": "",
     "userPassword": "",
 }
 
@@ -12,26 +13,34 @@ dataUser.forEach(data =>{
     data.addEventListener('input', validarCampo);
 });
 
+// form validation
+
 function validarCampo(e){
     let mensaje;
     let input = e.target;
     let inputData = e.target.value;
     if(inputData.trim().toLowerCase() === ''){
         let plcInput =  input.getAttribute('placeholder');
+        emailToSend[input.name] = '';
         mensaje = `${plcInput} cannot be empty`;
+        checkForm(emailToSend);
         showAlert(input, mensaje);
         return;
     }
 
     if(input.getAttribute('type') === 'email' && !checkEmail(inputData)){
         console.log('aaa')
+        emailToSend[input.name] = '';
         mensaje = 'Looks like this is not an email';
+        checkForm(emailToSend);
         showAlert(input, mensaje);
         return;
     }
 
     cleanAlert(input);
 
+    emailToSend[input.name] = inputData;
+    checkForm(emailToSend);
 }
 
 function checkEmail(email){
@@ -58,3 +67,17 @@ function cleanAlert(currInput){
     existAlert = currInput.parentElement.querySelector('.error-txt');
     existAlert? existAlert.remove(): true;
 }
+
+function checkForm(form){
+    let isDisabled = !Object.values(form).every(input => input !== '');
+    btnSend.classList.toggle('opacity-80', isDisabled);
+    btnSend.disabled = isDisabled;
+    console.log(form)
+}
+
+// sending the form
+
+btnSend.addEventListener('click', e=>{
+    e.preventDefault();
+    
+})
