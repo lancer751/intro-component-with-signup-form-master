1da055babf0b30c5f7e38567a071de9a6cdfc5f0
lancerlot751 <pachecolau27@gmail.com>
2024-04-01 22:42:51 -0500

    functions to validate the form has been added

--- src/js/script.js
+++ src/js/script.js
@@ -3,30 +3,58 @@ const dataUser = contactForm.querySelectorAll('input');
 
 const emailToSend = {
     "firstName": "",
-    "secondName": "",
+    "lastName": "",
     "userAddress": "",
-    "userEmail": "",
+    "userPassword": "",
 }
 
 dataUser.forEach(data =>{
-    data.addEventListener('blur', validarCampo);
-    console.log(data);
+    data.addEventListener('input', validarCampo);
 });
 
 function validarCampo(e){
+    let mensaje;
     let input = e.target;
     let inputData = e.target.value;
-    console.log(inputData);
-
     if(inputData.trim().toLowerCase() === ''){
-        showAlert(input);
+        let plcInput =  input.getAttribute('placeholder');
+        mensaje = `${plcInput} cannot be empty`;
+        showAlert(input, mensaje);
         return;
     }
+
+    if(input.getAttribute('type') === 'email' && !checkEmail(inputData)){
+        console.log('aaa')
+        mensaje = 'Looks like this is not an email';
+        showAlert(input, mensaje);
+        return;
+    }
+
+    cleanAlert(input);
+
 }
 
-function showAlert(referencia){
+function checkEmail(email){
+    const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    const result = regex.test(email);
+    return result;
+}
+
+function showAlert(currInput, txtMessage){
+    cleanAlert(currInput)
     errorTxt = document.createElement('P');
-    errorTxt.classList.add('text-Red', 'font-light', 'text-right', 'italic' ,'text-sm', 'mt-1');
-    errorTxt.textContent = 'hoafd';
-    referencia.parentElement.appendChild(errorTxt);
+    errorTxt.classList.add('error-txt');
+    errorTxt.textContent = txtMessage;
+    currInput.classList.remove('input-form');
+    currInput.classList.add('input-error');
+    currInput.parentElement.appendChild(errorTxt);
+    currInput.parentElement.querySelector('img').classList.remove('hidden');
+}
+
+function cleanAlert(currInput){
+    currInput.classList.remove('input-error');
+    currInput.classList.add('input-form');
+    currInput.parentElement.querySelector('img').classList.add('hidden');
+    existAlert = currInput.parentElement.querySelector('.error-txt');
+    existAlert? existAlert.remove(): true;
 }
